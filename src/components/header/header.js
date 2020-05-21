7aee87470484b9d4114546e2956eb0eb21182f3d
Prasan Kumar <prasan@moonraft.com>
2020-05-21 19:00:51 +0530

    Carousel Added

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Headerlogo from './headerlogo/headerlogo';
+import Caurosel from './caurosel/caurosel';
 const Header = (props) => {
     return (
         <header>
             <Headerlogo />
+            <Caurosel />
         </header>
     );
 }
