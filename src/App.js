fd4c1ad86afc7eb242f6216c830b706020ac943f
Prasan Kumar <prasan@moonraft.com>
2020-05-22 09:40:23 +0530

    Active Link added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import Header from './components/header/header';
 import Countrydata from './components/countrydata/countrydata';
 import Indiadata from './components/indiadata/indiadata';
@@ -8,9 +8,9 @@ function App() {
   return (
     <Router>
       <Header />
-      <Link to=""></Link>
-      <Link to="/country"></Link>
-      <Route exact path="/" component={Indiadata} />
+      <NavLink to="/india"></NavLink>
+      <NavLink to="/country"></NavLink>
+      <Route exact path="/india" component={Indiadata} />
       <Route path="/country" component={Countrydata} />
     </Router>
   );
