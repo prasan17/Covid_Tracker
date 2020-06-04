58db6a6303bb64079df9d25bb78456a3e514a656
Prasan Kumar <prasan@moonraft.com>
2020-06-04 13:41:47 +0530

    icon added

--- src/components/charts/charts.js
+++ src/components/charts/charts.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import './charts.scss';
 import LineCharts from './linecharts/linecharts';
-// import Doughnutchart from './doughnutchart/doughnutchart';
 function charts() {
     return (
         <main className="charts_wrapper">
             <LineCharts />
-            {/* <Doughnutchart /> */}
         </main>
     )
 }
